0392bf0cd6f92d3483a620fddd846c4ec644898a
Greg Weisbrod <greg.weisbrod@gmail.com>
2019-12-25 20:23:44 -0500

    add util module

---
+++ lib/xpring/util.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require_relative "./javascript"
+
+module Xpring
+  module Util
+    def self.valid_address?(address)
+      Javascript.run do
+        <<-JAVASCRIPT
+        EntryPoint.Utils.isValidAddress('#{address}');
+        JAVASCRIPT
+      end
+    end
+
+    def self.valid_x_address?(address)
+      Javascript.run do
+        <<-JAVASCRIPT
+        EntryPoint.Utils.isValidXAddress('#{address}');
+        JAVASCRIPT
+      end
+    end
+
+    def self.valid_classic_address?(address)
+      Javascript.run do
+        <<-JAVASCRIPT
+        EntryPoint.Utils.isValidClassicAddress('#{address}');
+        JAVASCRIPT
+      end
+    end
+
+    def self.encode(classic_address, tag: nil, test: false)
+      Javascript.run do
+        <<-JAVASCRIPT
+        EntryPoint.Utils.encodeXAddress('#{classic_address}', #{tag}, #{test});
+        JAVASCRIPT
+      end
+    end
+
+    def self.decode(x_address)
+      Javascript.run do
+        <<-JAVASCRIPT
+        EntryPoint.Utils.decodeXAddress('#{x_address}');
+        JAVASCRIPT
+      end
+    end
+  end
+end
+
